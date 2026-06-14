8b3ea79c15862b2260fef5014a843fd4b058e5dc
RogerReindeer <65850388+RogerReindeer@users.noreply.github.com>
2026-06-14 23:13:43 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+load_dotenv()
+
+app = FastAPI(title="Zefirki Reader Mini App")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "app_title": "Зефиркины баоцзы",
+        },
+    )
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
