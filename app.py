490ecb1fd25b6415309e340fea5ef5463e4b6744
RogerReindeer <65850388+RogerReindeer@users.noreply.github.com>
2026-06-15 00:24:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        name="index.html",
-        context={
-            "request": request,
+        request,
+        "index.html",
+        {
             "app_title": "Зефиркины баоцзы",
         },
     )
